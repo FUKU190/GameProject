f1d104641398b3329f0435d074f2e23ac221feba
Venn190 <famrry190@gmail.com>
2018-09-21 12:34:50 +0900

    second commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.AI;
+
+public class Controller : MonoBehaviour
+{
+
+    NavMeshAgent agent = null;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 入力を取得
+        var v1 = CrossPlatformInputManager.GetAxis("Vertical");
+        var h1 = CrossPlatformInputManager.GetAxis("Horizontal");
+
+       
+        // スティックが倒れていれば、移動
+        if (h1 != 0 || v1 != 0)
+        {
+            var direction = new Vector3(h1, 0, v1);
+            agent.Move(direction * Time.deltaTime);
+        }
+    }
+}
