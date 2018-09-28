1a55a0c0d1b4d99acbe1957b79fb6868a28a53aa
Venn190 <famrry190@gmail.com>
2018-09-28 10:26:06 +0900

    forth comit

---
+++ Assets/MyScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class MyScript : MonoBehaviour
+{
+
+    protected Joystick joystick;
+    protected JoyButton joyButton;
+    public float speed;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        joyButton = FindObjectOfType<JoyButton>();
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.velocity = new Vector3(joystick.Horizontal * speed,
+                                         rigidbody.velocity.y,
+                                         joystick.Vertical * speed);
+
+    
+    }
+}
